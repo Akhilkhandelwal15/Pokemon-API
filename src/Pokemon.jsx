286467b4162f0edb3de4c50dd2688d32848dab01
Akhilkhandelwal15 <khandelwalakhil2003@gmail.com>
2025-07-05 11:32:08 +0530

    Some improvements and optimizations

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -6,33 +6,29 @@ export const Pokemon = ()=>{
     const [pokemons, setPokemons] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [search, setSearch] = useState("");
     
     const API = "https://pokeapi.co/api/v2/pokemon";
 
     const fetchPokemons = async()=>{
+        setLoading(true);
         try{
             const res = await fetch(API);
             const data = await res.json();
-            // console.log(data);
 
             const detailedPokemonData = data.results.map(async(item)=>{
-                // console.log(item.url);
                 try{
                     const res = await fetch(item.url);
                     const data = await res.json();
-                    // console.log(data);
                     return data;
                 }
                 catch(error){
                     console.log("error", error);
-                    setLoading(false);
-                    setError(error.message);
+                    return null;
                 }
             });
-            // console.log(detailedPokemonData);
 
             const allPokeonsData = await Promise.all(detailedPokemonData);
-            console.log('->>>',allPokeonsData);
             setPokemons(allPokeonsData);
             setLoading(false);
         }
@@ -44,21 +40,23 @@ export const Pokemon = ()=>{
     }
 
     useEffect(()=>{
-        setLoading(true);
         fetchPokemons();
     }, []);
+
+    const filteredData = pokemons.filter((item)=> item.name.toLowerCase().includes(search.toLowerCase().trim()));
 
     return (
         <div className="main-container">
             <h2>Explore your favourite pokemons</h2>
+            <input type="text" placeholder="Search..." value={search} onChange={(event)=>setSearch(event.target.value)}/>
             {loading? <p>Loading...</p> : error ? <p>Error while fetching data {error} </p> : (
                 <div className="cards">
-                        {pokemons.map((pokemon)=>(
+                        {filteredData.map((pokemon)=>(
                             <PokemonCards key={pokemon.id} pokemon={pokemon} />
                         ))}
                 </div>
             )}
-            
+            {!loading && !error && filteredData.length===0 && <h1>No Results Found</h1>}
         </div>
     );
 }
